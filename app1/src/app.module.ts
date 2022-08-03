68041a97fc94f367f9f516cad3e8644575996d20
GustavoSMelo <gsantos15569@gmail.com>
2022-08-02 21:49:46 -0300

    :recycle: new modules added in app module

--- app1/src/app.module.ts
+++ app1/src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { CoursesModule } from './modules/courses/courses.module';
+import { DatabaseModule } from './modules/database/database.module';
 
 @Module({
-    imports: [],
+    imports: [ConfigModule.forRoot(), DatabaseModule, CoursesModule],
 })
 export class AppModule {}
