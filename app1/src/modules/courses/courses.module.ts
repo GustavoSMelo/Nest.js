61bda17085e2228f6cd9e5ff3123dd1a0f361f94
GustavoSMelo <gsantos15569@gmail.com>
2022-08-08 20:29:07 -0300

    :recycle: update import

--- app1/src/modules/courses/courses.module.ts
+++ app1/src/modules/courses/courses.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Tags } from '../tags/entities/tags.entity';
+import { TagsModule } from '../tags/tags.module';
 import { CoursesController } from './controller/courses.controller';
 import { Courses } from './entities/courses.entity';
-import { CoursesService } from './services/courses.services';
+import { CoursesService } from './services/courses.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Courses])],
+    imports: [TypeOrmModule.forFeature([Courses]), TagsModule],
     controllers: [CoursesController],
     providers: [CoursesService],
 })
