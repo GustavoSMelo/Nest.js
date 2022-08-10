4ce756fef3ee3d21b275958e4234317318175d16
GustavoSMelo <gsantos15569@gmail.com>
2022-08-09 21:34:44 -0300

    :recycle: add ormconfig to main.ts

--- app1/src/main.ts
+++ app1/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { connectionSource } from './modules/config/ormconfig';
+
 dotenv.config();
 
 const bootstrap = async () => {
+    await connectionSource.initialize();
     const app = await NestFactory.create(AppModule);
     await app.listen(3000);
 };
